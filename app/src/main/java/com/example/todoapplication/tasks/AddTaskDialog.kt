823250e3f0c6e9b7899f29d62b68bf70e5bd586b
t-okada <t-okada@atware.co.jp>
2020-01-29 08:34:52 +0900

    ダイアログに入力した値をTOASTで表示する

--- app/src/main/java/com/example/todoapplication/tasks/AddTaskDialog.kt
+++ app/src/main/java/com/example/todoapplication/tasks/AddTaskDialog.kt
@@ -2,11 +2,14 @@ package com.example.todoapplication.tasks
 
 import android.app.Dialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
-
+import androidx.fragment.app.Fragment
 import com.example.todoapplication.R
+import com.example.todoapplication.databinding.FragmentAddTaskDialogBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,11 +17,16 @@ import com.example.todoapplication.R
 class AddTaskDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(context!!)
-        val inflater = requireActivity().layoutInflater
 
-        builder.setView(inflater.inflate(R.layout.fragment_add_task_dialog, null))
+        val binding = DataBindingUtil.inflate<FragmentAddTaskDialogBinding>(LayoutInflater.from(activity), R.layout.fragment_add_task_dialog, null, false)
+        val view = binding.root
+
+        builder.setView(view)
             .setTitle(R.string.add_task)
-            .setPositiveButton(R.string.add) { dialog, _ -> dialog.dismiss() }
+            .setIcon(R.mipmap.ic_launcher)
+            .setPositiveButton(R.string.add) { dialog, _ ->
+                Toast.makeText(activity, binding.taskName.text, Toast.LENGTH_SHORT).show()
+            }
             .setNegativeButton(R.string.close) { dialog, _ -> dialog.dismiss() }
 
         return builder.create()
